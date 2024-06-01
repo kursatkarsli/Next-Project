2a8fb7986035812ca6a02d123520f405d0b6be90
Kürşat Karslı <kursat.karsli@hotmail.com>
2024-06-01 17:05:10 +0300

    cookie fixed

--- src/app/packages/page.tsx
+++ src/app/packages/page.tsx
@@ -3,9 +3,7 @@ import Packages from "./PackageComponent";
 import { cookies } from "next/headers";
 
 function PackageComponent() {
-  if (typeof window !== "undefined") {
-    cookies().set("token", localStorage.getItem("token")!);
-  }
+
   return <Packages />;
 }
 
